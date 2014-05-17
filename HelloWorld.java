7122411dd7b41ae8878f583421ea07d2e44b1e6b
tailedge <tailedge24@gmail.com>
2014-05-17 17:55:11 +0900

    add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+	new HelloWorld().execute();
+    }
+
+    public void execute() {
+	System.out.println("Hello, World!");
+    }
+}
