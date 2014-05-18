19ef6e7b381f55bb944e128fac45933e3a12af28
tailedge <tailedge24@gmail.com>
2014-05-18 16:47:01 +0900

    Add comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,17 @@
+/**
+ * HelloWorld.java
+ */
 public class HelloWorld {
+    /**
+     * main
+     */
     public static void main(String[] args) {
 	new HelloWorld().execute();
     }
 
+    /**
+     * execute
+     */
     public void execute() {
 	System.out.println("Hello, World!");
     }
